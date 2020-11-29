aefea45fb13be10f3f67d067f73e8eb1912f0c5f
FaPra_02 FaPra_02 <fapra_02@hcics.simtech.uni-stuttgart.de>
2020-11-29 22:17:09 +0100

    Test

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ if __name__ == '__main__':
 
         return cropped_image
 
-    def random_jitter(image):
+    def random_jitter(image)
+        print(image)
         # resizing to 286 x 286 x 3
         image = tf.image.resize(image, [286, 286],
                                 method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
@@ -53,6 +54,7 @@ if __name__ == '__main__':
     def parse_func(filename):
         image_string = tf.io.read_file(filename)  # Read the image
         image_decoded = tf.image.decode_jpeg(image_string)  # Decode the image
+        print(image_decoded)
         image = tf.image.convert_image_dtype(image_decoded, tf.float32)  # Normalize the image
         return image
 
