9f1e20602a6af23f3aeed3f659667624e2059e64
FaPra_02 FaPra_02 <fapra_02@hcics.simtech.uni-stuttgart.de>
2020-11-29 14:41:36 +0100

    Added tensorflow Compatible changes and removed previous incompatible change

--- main.py
+++ main.py
@@ -1,53 +1,70 @@
-import argparse
-import os
 import tensorflow as tf
-tf.random.set_seed(19)
-from model import cyclegan
-
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('--dataset_dir', dest='dataset_dir', default='horse2zebra', help='path of the dataset')
-parser.add_argument('--epoch', dest='epoch', type=int, default=200, help='# of epoch')
-parser.add_argument('--epoch_step', dest='epoch_step', type=int, default=100, help='# of epoch to decay lr')
-parser.add_argument('--batch_size', dest='batch_size', type=int, default=1, help='# images in batch')
-parser.add_argument('--train_size', dest='train_size', type=int, default=1e8, help='# images used to train')
-parser.add_argument('--load_size', dest='load_size', type=int, default=286, help='scale images to this size')
-parser.add_argument('--fine_size', dest='fine_size', type=int, default=256, help='then crop to this size')
-parser.add_argument('--ngf', dest='ngf', type=int, default=64, help='# of gen filters in first conv layer')
-parser.add_argument('--ndf', dest='ndf', type=int, default=64, help='# of discri filters in first conv layer')
-parser.add_argument('--input_nc', dest='input_nc', type=int, default=3, help='# of input image channels')
-parser.add_argument('--output_nc', dest='output_nc', type=int, default=3, help='# of output image channels')
-parser.add_argument('--lr', dest='lr', type=float, default=0.0002, help='initial learning rate for adam')
-parser.add_argument('--beta1', dest='beta1', type=float, default=0.5, help='momentum term of adam')
-parser.add_argument('--which_direction', dest='which_direction', default='AtoB', help='AtoB or BtoA')
-parser.add_argument('--phase', dest='phase', default='train', help='train, test')
-parser.add_argument('--save_freq', dest='save_freq', type=int, default=1000, help='save a model every save_freq iterations')
-parser.add_argument('--print_freq', dest='print_freq', type=int, default=100, help='print the debug information every print_freq iterations')
-parser.add_argument('--continue_train', dest='continue_train', type=bool, default=False, help='if continue training, load the latest model: 1: true, 0: false')
-parser.add_argument('--checkpoint_dir', dest='checkpoint_dir', default='./checkpoint', help='models are saved here')
-parser.add_argument('--sample_dir', dest='sample_dir', default='./sample', help='sample are saved here')
-parser.add_argument('--test_dir', dest='test_dir', default='./test', help='test sample are saved here')
-parser.add_argument('--L1_lambda', dest='L1_lambda', type=float, default=10.0, help='weight on L1 term in objective')
-parser.add_argument('--use_resnet', dest='use_resnet', type=bool, default=True, help='generation network using reidule block')
-parser.add_argument('--use_lsgan', dest='use_lsgan', type=bool, default=True, help='gan loss defined in lsgan')
-parser.add_argument('--max_size', dest='max_size', type=int, default=50, help='max size of image pool, 0 means do not use image pool')
-
-args = parser.parse_args()
-
-
-def start():
-    if not os.path.exists(args.checkpoint_dir):
-        os.makedirs(args.checkpoint_dir)
-    if not os.path.exists(args.sample_dir):
-        os.makedirs(args.sample_dir)
-    if not os.path.exists(args.test_dir):
-        os.makedirs(args.test_dir)
-
-    tfconfig = tf.compat.v1.ConfigProto(allow_soft_placement=True)
-    tfconfig.gpu_options.allow_growth = True
-    with tf.compat.v1.Session(config=tfconfig) as sess:
-        model = cyclegan(sess, args)
-        model.train(args) if args.phase == 'train' \
-            else model.test(args)
 
 if __name__ == '__main__':
-    start()
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+    dataset, metadata = None# Load Dataset here
+
+    train_horse, train_zebra = DataSet(dataset['trainA'], dataset['trainB'])
+    test_horse, test_zebra = DataSet(dataset['testA'], dataset['testB'])
+
+    BUFFER_SIZE = 1000
+    BATCH_SIZE = 1
+    IMG_WIDTH = 256
+    IMG_HEIGHT = 256
+
+
+    def random_crop(image):
+        cropped_image = tf.image.random_crop(
+            image, size=[IMG_HEIGHT, IMG_WIDTH, 3])
+
+        return cropped_image
+
+
+    # normalizing the images to [0, 1]
+    def normalize(image):
+        image = tf.cast(image, tf.float32)
+        image = (image / 255)
+        return image
+
+
+    def random_jitter(image):
+        # resizing to 286 x 286 x 3
+        image = tf.image.resize(image, [286, 286],
+                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+
+        # randomly cropping to 256 x 256 x 3
+        image = random_crop(image)
+
+        # random mirroring
+        image = tf.image.random_flip_left_right(image)
+
+        return image
+
+
+    def preprocess_image_train(image, label):
+        image = random_jitter(image)
+        image = normalize(image)
+        return image
+
+
+    def preprocess_image_test(image, label):
+        image = normalize(image)
+        return image
+
+
+    train_horses = train_horse.map(
+        preprocess_image_train, num_parallel_calls=AUTOTUNE).cache().shuffle(
+        BUFFER_SIZE).batch(1)
+
+    train_zebras = train_zebra.map(
+        preprocess_image_train, num_parallel_calls=AUTOTUNE).cache().shuffle(
+        BUFFER_SIZE).batch(1)
+
+    test_horses = test_horse.map(
+        preprocess_image_test, num_parallel_calls=AUTOTUNE).cache().shuffle(
+        BUFFER_SIZE).batch(1)
+
+    test_zebras = test_zebra.map(
+        preprocess_image_test, num_parallel_calls=AUTOTUNE).cache().shuffle(
+        BUFFER_SIZE).batch(1)
